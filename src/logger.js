9dc11acae1c2cc8b87eb9f0db217257807c21ab6
Uny Li <sli359@myseneca.ca>
2025-09-05 16:08:57 -0400

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,7 @@
 // src/logger.js
 
+const pino = require('pino');
+
 // Use `info` as our standard log level if not specified
 const options = { level: process.env.LOG_LEVEL || 'info' };
 
@@ -14,6 +16,18 @@ if (options.level === 'debug') {
   };
 }
 
-// Create and export a Pino Logger instance:
-// https://getpino.io/#/docs/api?id=logger
-module.exports = require('pino')(options);
+// // Create and export a Pino Logger instance:
+// // https://getpino.io/#/docs/api?id=logger
+// module.exports = require('pino')(options);
+
+// Create the logger instance
+const logger = pino(options);
+
+// If debug mode, print all environment variables once
+if (process.env.LOG_LEVEL === 'debug') {
+  logger.debug('--- Environment Variables ---');
+  logger.debug(process.env);
+  logger.debug('-----------------------------');
+}
+
+module.exports = logger;
