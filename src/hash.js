eb8d3736339bb9b36906cd1859502ab4f2b04a0a
Uny Li <sli359@myseneca.ca>
2025-10-10 21:09:30 -0400

    update hash.js

--- src/hash.js
+++ src/hash.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const algorithm = 'sha256';
 const secret = process.env.HASH_SECRET || 'default-secret';
 
-function hash(email) {
+function hash (email) {
   if (email === null || email === undefined || typeof email !== 'string') {
     throw new Error('Email must be a string');
   }
